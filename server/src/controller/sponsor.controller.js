43ad34cb8d1fe8442a75e2ec707db9ba179c3a82
SHARMA-ANK <ankitsharma_22248@aitpunr.edu.in>
2025-02-13 00:39:13 +0530

    automation removed

--- server/src/controller/sponsor.controller.js
+++ server/src/controller/sponsor.controller.js
@@ -53,48 +53,7 @@ const getDetails = asyncHandler(async (req, res) => {
   
 
   // Construct the email subject and body
-  const subject = " Thank you for showing interest in sponsoring PACE 2025! ";
-  const htmlMessage = `
-    
-  <div style="font-family: Arial, sans-serif; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px; max-width: 600px; margin: auto;">
-    <h2 style="text-align: center; color: #007BFF;"> Thank you for showing interest in sponsoring PACE 2025! </h2>
-    <b>Every contribution, big or small, helps us create an unforgettable experience for AITians.</b>
-    <p>Your support strengthens the spirit, and keeps the legacy alive. We truly appreciate your generosity in making this event bigger and better! 🏆</p>
-    
-    <div style="text-align: center; margin: 20px 0;">
-        <b> Scan the QR code below to make your contribution and be a part of this incredible journey! </b>
-        <br/><br/>
-        <img src="${qrCode}" alt="QR Code" style="width: 300px; height: 400px;" />
-        <br/>
-    </div>
-    
-    <b>📩 After making the payment, please revert back to us on this email with a screenshot of the transaction page for confirmation.</b>
-    
-    
-    <hr style="border: 0; height: 1px; background: #ddd; margin: 20px 0;">
-    
-    <p style="text-align: center; color: #333;">
-        <b>Warm Regards,</b><br/>
-        <b>AIT Sports Club</b><br/>
-        📞 +91 9347972885 <br/>
-        📩 <a href="mailto:pace2k25@gmail.com" style="color: #007BFF; text-decoration: none;">pace2k25@gmail.com</a><br/>
-        🌐 <a href="https://www.pace2025.in" style="color: #007BFF; text-decoration: none;">www.pace2025.in</a><br/>
-        <br/>
-        Let’s make PACE 2025 unforgettable, together. 
-    </p>
-</div>
-
-
-
-  `;
-
-  try {
-    await sendEmail(email, subject, htmlMessage);
-  } catch (error) {
-    // Forward the error with your custom ApiError
-    throw new ApiError(500, error.message || "Email could not be sent");
-  }
-
+  
   res.status(200).json({
     success: true,
     message: "Sponsorship request received. An email has been sent.",
